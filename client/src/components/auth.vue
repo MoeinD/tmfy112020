2370b2de6a0841e9dc95a465d46aa475cab77b5f
moeinD <karimi.mohammad1977@gmail.com>
2020-12-02 09:17:38 +1000

    Add some more refactoring in the login page and use some more flag to use for the register page as well.

--- client/src/components/auth.vue
+++ client/src/components/auth.vue
@@ -1,17 +1,29 @@
 <template>
   <div>
-    <div class="modal-common auth">
+    <div class="modal-common auth" :class="{'login' : isloginModal}">
       <div class="aut-content">
+        <div v-if="!isloginModal">
+          <div class="form-control">
+            <input type="text" class="auth-input-control" placeholder="First Name" />
+          </div>
+          <div class="form-control">
+            <input type="text" class="auth-input-control" placeholder="Last Name" />
+          </div>
+        </div>
         <div class="form-control">
           <input type="text" class="auth-input-control" placeholder="email" />
         </div>
         <div class="form-control">
           <input type="password" class="auth-input-control" placeholder="password" />
         </div>
-        <button class="button aut-botton">login</button>
+        <button class="button aut-botton" @click="login()">{{isloginModal ? `login` : 'signup'}}</button>
         <div class="auth-footer">
-          <label class="auth-ask">Not registered?</label>
-          <label for class="hyperlink">Create an account</label>
+          <label class="auth-ask">{{isloginModal ? 'Have an account' : 'Not registered?'}}</label>
+          <label
+            for
+            class="hyperlink"
+            @click="toggleToRegister"
+          >{{isloginModal ? 'Back To Login' : 'Create an account'}}</label>
         </div>
       </div>
     </div>
@@ -24,7 +36,22 @@ import * as mockupData from "../assets/mockupData";
 import commonMethod from "./commonMethod";
 import mixins from "vue-typed-mixins";
 export default mixins(commonMethod).extend({
-  methods: {}
+  data() {
+    return {
+      isloginModal: true,
+      authObject: {
+        firstName: null,
+        lastName: null,
+        email: null,
+        password: null
+      }
+    };
+  },
+  methods: {
+    toggleToRegister() {
+      this.isloginModal = !this.isloginModal;
+    }
+  }
 });
 </script>
 <style lang="scss" scoped>
